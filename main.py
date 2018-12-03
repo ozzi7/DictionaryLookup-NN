07686916ba6f583b5c4152cb9f89eff94a712dad
ozzi7 <nielregen@gmail.com>
2018-12-03 23:27:12 +0100

    Simplification to chain of 1 key dictionaries

--- main.py
+++ main.py
@@ -23,62 +23,62 @@ get_custom_objects().update({'gaussian': Activation(gaussian)})
 # create samples
 def create_data(nof_samples, nof_dict_entries, min_val=0, max_val=1):
 
-    data_X = np.random.rand(nof_samples * (nof_dict_entries // 2 * 5))*(max_val-min_val)+min_val
-    data_y = np.random.rand(nof_samples) * (nof_dict_entries // 2)*(max_val-min_val)+min_val
+    data_X = np.random.rand(nof_samples * (nof_dict_entries * 3))*(max_val-min_val)+min_val
+    data_y = np.random.rand(nof_samples) * (nof_dict_entries)*(max_val-min_val)+min_val
 
     for i in range(0, nof_samples):
-        index = random.randint(0,nof_dict_entries) # 1/nof_dict_entries chance of no key in dict, output 0
+        index = random.randint(0,nof_dict_entries) # no key in dict output 0
         if index < nof_dict_entries:
-            for j in range(0, nof_dict_entries // 2):
-                data_X[i*(5*nof_dict_entries//2) + j*5+4] = data_X[i*(5*nof_dict_entries//2) + (index // 2)*5+ 2*(index % 2)]
-            data_y[i] = data_X[i*(5*nof_dict_entries//2) + (index // 2)*5+ 2*(index % 2)+1]
+            for j in range(0, nof_dict_entries):
+                data_X[i*(3*nof_dict_entries) + j*3 + 2] = data_X[i*(3*nof_dict_entries) + index*3]
+            data_y[i] = data_X[i*(3*nof_dict_entries) + index*3 + 1]
         else:
             key = random.uniform(min_val, max_val)
-            for j in range(0, nof_dict_entries // 2):
-                data_X[i*(5*nof_dict_entries//2) + j * 5 + 4] = key
+            for j in range(0, nof_dict_entries):
+                data_X[i*(3*nof_dict_entries) + j * 3 + 2] = key
             data_y[i] = 0.0
 
-    return np.reshape(data_X,[nof_samples,(nof_dict_entries//2)*5]), np.reshape(data_y,[nof_samples,1])
+    return np.reshape(data_X,[nof_samples,(nof_dict_entries)*3]), np.reshape(data_y,[nof_samples,1])
 
 
 nof_samples = 50
-nof_dict_entries = 20 # let this be a multiple of 2 for now
+nof_dict_entries = 4 # let this be a multiple of 2 for now
 min_val = 0
 max_val = 20
 
 train_X, train_y = create_data(nof_samples, nof_dict_entries, min_val, max_val)
 
-input = Input(shape=(nof_dict_entries//2*5,))
+input = Input(shape=(nof_dict_entries*3,))
 
 print(input)
 outputs = []
 # Create a larger dictionary out of the small ones
-for i in range(nof_dict_entries//2):
+for i in range(nof_dict_entries):
     first_layer = []
-    out = Lambda(lambda x: x[:, i*5:i*5+5])(input)  # get 5 values of input
+    out = Lambda(lambda x: x[:, i*3:i*3+3])(input)  # get 3 values of input
 
-    first_layer.append(Dense(2, activation="linear",
-                             weights=[np.array([[0, 0], [1, 0], [0, 0], [0, 1], [0, 0]]), np.array([0, 0])],
+    first_layer.append(Dense(1, activation="linear",
+                             weights=[np.array([[0], [1], [0]]), np.array([0])],
                              trainable=False)(out)) # to forward values
-    first_layer.append(Dense(2, activation="gaussian",
-                             weights=[np.array([[10,0],[0,0],[0,10],[0,0],[-10,-10]]), np.array([0,0])],
+    first_layer.append(Dense(1, activation="gaussian",
+                             weights=[np.array([[10],[0],[-10]]), np.array([0])],
                              trainable=False)(out))  # output high value for match, 0 otherwise
     concatenate_l = concatenate(first_layer)
-    outputs.append(Dense(2, activation='relu',
-                         weights= [np.array([[1,0],[0,1],[20,0], [0,20]]), np.array([-20,-20])],
+    outputs.append(Dense(1, activation='relu',
+                         weights= [np.array([[1],[20]]), np.array([-20])],
                          trainable=True)(concatenate_l))  # subtract high value to cut off non-matching
 
-if nof_dict_entries > 2:
+if nof_dict_entries > 1:
     concatenate_outputs_l = concatenate(outputs)
     output_l = Dense(1, activation='linear', weights=[np.ones([nof_dict_entries,1]), np.zeros(1)],
                      trainable=False)(concatenate_outputs_l)  # sum up the two elements, identity
 else:
-    output_l = Dense(1, activation='linear', weights=[np.array([[1],[1]]), np.array([0])], trainable=False)(outputs[0])
+    output_l = Dense(1, activation='linear', weights=[np.array([[1]]), np.array([0])], trainable=False)(outputs[0])
 
 model = Model(inputs=[input], outputs=[output_l])
 model.compile(optimizer=Adam(lr=0.004, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False), loss='mae')
 model.summary()
-model.fit(train_X, train_y, epochs=40000, batch_size=1000)
+model.fit(train_X, train_y, epochs=1000, batch_size=1000)
 
 print(model.layers[0].get_weights())
 print(model.layers[1].get_weights())
